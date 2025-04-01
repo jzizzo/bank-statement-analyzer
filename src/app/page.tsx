1f529ef2a7f388c2d39111c438a8b137262c93fd
Joseph Zizzo <hire.jzizzo@gmail.com>
2025-04-01 11:48:42 -0700

    Remove unnecessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import FileUpload from "../components/ui/FileUpload";
 import { RawStatementData, ProcessedStatement } from "../lib/types";
